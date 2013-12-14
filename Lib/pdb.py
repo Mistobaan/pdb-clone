8dbf9bb2e53e1b6bb52d87506a8e7595e756aab9
Xavier de Gaye <xdegaye@users.sourceforge.net>
2013-12-14 15:30:52 +0100

    Add tests for pdb.find_function.

--- Lib/pdb.py
+++ Lib/pdb.py
@@ -97,18 +97,11 @@ def find_function(funcname, filename):
     except IOError:
         return None
     # consumer of this info expects the first line to be 1
-    lineno = 1
-    answer = None
-    while True:
-        line = fp.readline()
-        if line == '':
-            break
-        if cre.match(line):
-            answer = funcname, filename, lineno
-            break
-        lineno += 1
-    fp.close()
-    return answer
+    with fp:
+        for lineno, line in enumerate(fp, start=1):
+            if cre.match(line):
+                return funcname, filename, lineno
+    return None
 
 def getsourcelines(obj, locals=None):
     lines, lineno = inspect.findsource(obj)
